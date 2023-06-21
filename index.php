1d54507b1b1324ade7f0a9015d695859b30a9964
Antonio <antonioboksic1603@gmail.com>
2023-06-21 19:39:41 +0200

    bonus finiti: filtro voto completato, possibile utilizzare entrambi i filtri insieme.

--- index.php
+++ index.php
@@ -56,20 +56,41 @@
 
         // mi salvo input dell'utente del parcheggio
         $parking = $_GET["parking"];
+        echo "input parcheggio:";
         var_dump($parking);
+
+        // mi salvo input dell'utente del parcheggio
+        $vote = $_GET["vote"] ?? -1;
+        // qua con ?? -1 gli vado a dire che se il valore esiste me lo tengo, se il valore è null me lo sosstituisci con -1, funzionava anche senza perchè evidentemente il null che tornava dalle condizioni veniva interpretato come 0, ma così è più chiaro
+        echo "input voto:";
+        var_dump($vote);
 
 
     ?>
 
     <!-- creo form -->
     <form>
-        <!-- parcheggio -->
+        <!-- filtro parcheggio -->
         <label for="Parcheggio">Parcheggio</label>
         <input type="radio" id="parking_yes" name="parking" value="yes">
         <label for="parking_yes">Yes</label>
         <input type="radio" id="parking_no" name="parking" value="no">
         <label for="parking_no">No</label>
 
+        <!-- filtro stelle albergo -->
+        <br>
+        <label for="vote">vote</label>
+        <select name="vote" id="vote">
+            <option value="-1">seleziona voto minimo</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+
+        <!-- bottone submit che uso per entrambi i filtri/input -->
+        <br>
         <input type="submit" value="SEARCH">
 
     </form>
@@ -86,11 +107,21 @@
         <?php
         
             foreach ($hotels as $hotel) {
-                if ($parking === null 
-                || ($parking === "yes" && $hotel['parking'])
-                || ($parking === "no" && !$hotel['parking'])
+                // if (
+                // $parking === null &&  $vote <= $hotel["vote"]
+                // || ($parking === "yes" && $hotel['parking']) &&  $vote <= $hotel["vote"]
+                // || ($parking === "no" && !$hotel['parking']) &&  $vote <= $hotel["vote"]
                  
-                ) {
+                // )
+                
+                // si può sintetizzare anche così quando vogliamo aggiungere un secondo filtro che si applica in parallelo a tutte le altre condizioni già impostate
+                if (
+                ($parking === null 
+                || ($parking === "yes" && $hotel['parking']) 
+                || ($parking === "no" && !$hotel['parking']) 
+                )&&  $vote <= $hotel["vote"]
+                 
+                ){
                     echo "<tr>";
                     echo "<td>" . $hotel["name"] . " " . "</td>";
                     echo "<td>" . $hotel["description"] . " " . "</td>";
